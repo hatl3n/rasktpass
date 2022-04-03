1f041beb12ed2eae6558a8d69b59357c3e0eadcc
hatl3n <djhatlen@gmail.com>
2022-04-03 21:14:22 +0200

    Edit 8080-80. Add npm start

--- express.js
+++ express.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const rasktpass = require('./index.js');
 const app = express();
-const port = 8080;
+const port = 80;
 
 app.get('/getObj', async (req, res) => {
   let rp = await rasktpass();
