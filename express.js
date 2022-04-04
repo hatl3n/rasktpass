d694a3d12b5e56b0616dac96ab81f5f5cd25f2ca
hatl3n <102922758+hatl3n@users.noreply.github.com>
2022-04-04 08:01:05 +0200

    Update express.js
    Implemented superb caching

--- express.js
+++ express.js
@@ -3,8 +3,23 @@ const rasktpass = require('./index.js');
 const app = express();
 const port = 80;
 
+var cachedJson = [0, ""]; // Format: unixtimems, jsonstring
+const _10MIN = 600000; // 10 min in ms
+
 app.get('/getObj', async (req, res) => {
-  let rp = await rasktpass();
+  let now = () => new Date().getTime();
+  let toRespond;
+  if ((cachedJson[0] + _10MIN) < now())
+  {
+    let rp = await rasktpass();
+    cachedJson = [now(), rp];
+    toRespond = rp;
+    console.log("Stored new cache");
+  }
+  else {
+    toRespond = cachedJson[1];
+    console.log("Served from cache");
+  }
   res.send(JSON.stringify(rp));
 })
 
